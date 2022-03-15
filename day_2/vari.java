5ae452ce142f46c695d92d45dffc92d23352811d
smriti6 <smritis481@gmail.com>
2022-03-15 20:32:52 +0530

    second

---
+++ day_2/vari.java
@@ -0,0 +1,8 @@
+class vari {
+    public static void main(String args []) {
+        System.out.println("Day 2");
+        int a=3;
+        String b="xyz";
+        System.out.println(a+" "+b);
+    }
+}
