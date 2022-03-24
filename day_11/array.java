73b56b419f528f242fbb310b11a1af35acb23c3f
smriti6 <smritis481@gmail.com>
2022-03-24 21:36:59 +0530

    11

---
+++ day_11/array.java
@@ -0,0 +1,11 @@
+class array {
+    public static void main (String args []) {
+        System.out.println("Day 11");
+        System.out.println("Array");
+        int [] a= {1,2,3,4,5};
+        for(int i=0;i<=4;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
