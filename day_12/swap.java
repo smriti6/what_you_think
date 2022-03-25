399b8972c5bbfcb7dc706281174b452e532ff2f6
smriti6 <smritis481@gmail.com>
2022-03-25 22:43:47 +0530

    12

---
+++ day_12/swap.java
@@ -0,0 +1,13 @@
+class swap {
+    public static void main(String[] args) {
+        System.out.println("day 12");
+        System.out.println("Swap 2 number");
+        int n1=5;
+        int n2=10;
+        System.out.println("Before swap n1 = "+n1 +"  n2 = "+n2);
+        int t=n1;
+        n1=n2;
+        n2=t;
+        System.out.println("After swap n1 = "+n1 +"  n2 = "+n2);
+    }
+}
