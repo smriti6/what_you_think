97f71d2370b56c46a163644c74e75eb8efc52531
smriti6 <smritis481@gmail.com>
2022-03-31 23:41:15 +0530

    17

--- Practise_math/start_math.java
+++ Practise_math/start_math.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
 public class start_math {
+    static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        
+       // count();
+       palindrome();
+
+    }   
+
+    static void count()
+    {
         System.out.println("Count digit");
         System.out.println("Enter number");
         int n=sc.nextInt();
         int t=0;
-        System.out.println("\n\n");
+        System.out.println("\n");
         while(n>0)
         {
             n=n/10; //divide number by 10 so it decrese by 1 or last number is deleted 
@@ -16,5 +22,27 @@ public class start_math {
             t++; //when a number is deleted t is increse by 1.
         }
         System.out.println(t);
-    }   
+    }
+
+    static void palindrome()
+    {
+        System.out.println("palindrome number");
+        int n=sc.nextInt();
+        int a=n;
+        int t=0;
+        while(a>0)
+        {
+            t=t*10+a % 10;
+            a=a/10;
+        }
+        System.out.println(t);
+        if(n==t)
+        {
+            System.out.println("palindrome");
+        }
+        else
+        {
+            System.out.println("not");
+        }
+    }
 }
