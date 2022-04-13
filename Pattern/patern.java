b5a3ea67f68533904979d1263f57d61f7b869af2
smriti6 <smritis481@gmail.com>
2022-04-14 00:06:19 +0530

    28

---
+++ Pattern/patern.java
@@ -0,0 +1,17 @@
+public class patern {
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=0;i<=n;i++)
+        {
+            for(int k=n-i;k>0;k--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
