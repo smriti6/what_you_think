3275a711aa034f014acd5d087fe2dfa84d4465af
smriti6 <smritis481@gmail.com>
2022-05-03 21:30:43 +0530

    commi

--- Practise_2/start.java
+++ Practise_2/start.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+  
 public class start {
     public static void main(String[] args) {
         System.out.println("working");
+        float f=9.0;
+        System.out.println(f/(float)3);
     }
+        
 }
