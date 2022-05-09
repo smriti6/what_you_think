c203db6089ed16613f721c2ef2c186ffb70d7640
smriti6 <smritis481@gmail.com>
2022-05-09 23:16:50 +0530

    co

---
+++ Practise_2/re.java
@@ -0,0 +1,12 @@
+public class re {
+    public static void main(String[] args) {
+        int n=9713;
+        int t=0;
+        while(n!=0)
+        {
+            n=n/10;
+            t++;
+        }
+        System.out.println(t);
+    }
+}
