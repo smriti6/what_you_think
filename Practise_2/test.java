63a58927e2a651ee178dacc072e4f2063f5b5f68
smriti6 <smritis481@gmail.com>
2022-05-11 22:39:45 +0530

    co

--- Practise_2/test.java
+++ Practise_2/test.java
@@ -7,5 +7,43 @@ class test {
         {
             System.out.println(name);
         }
+        
+
+
+
+
+    
+    
+    int a[]={1,3,5};
+    for(int b:a)
+    {
+        System.out.print(++b);
     }
-}
+
+    // int a=3;
+    // int b=a<<2;
+    // int c=b>>1;
+    // System.out.println(c);
+
+
+    // int a=4%2*3-1/0;
+    // System.out.println(a);
+
+    // int i = 1;
+    // for (; i <5; i += 2) {
+    // System.out.print(i + "");
+    // }
+    // System.out.println(i);
+
+    // int x = 1;
+    // int y = 6;
+    // while (y--) {
+    // x++;
+    // }
+    // System.out.println("x =" + x + "y =" +y); 
+ 
+
+}}
+
+
+
