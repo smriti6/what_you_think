2ae11da7ca20cef8119e564380d5cd5e20de64b8
smriti6 <smritis481@gmail.com>
2022-06-23 21:48:51 +0530

    Array

---
+++ Array/reverseArray.java
@@ -0,0 +1,9 @@
+public class reverseArray {
+    public static void main(String[] args) {
+        int n=5;
+        int arr[]={1,2,3,4,5,6};
+        for(int i=n;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
