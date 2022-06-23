3d1ce4a085faeca61a27f9ddcac268de82c5c05c
smriti6 <smritis481@gmail.com>
2022-06-23 22:55:46 +0530

    Array

---
+++ Array/secSmall.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class secSmall {
+    public static void main(String[] args) {
+        System.out.println("Write a Java program to find the second smallest element in an array. ");
+        int arr[]={45,7563,343,214,989,654,2,6,7};
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
